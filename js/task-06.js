e069ffd091019ce5cbde49e3551511e3fbdbe613
Pavlina Antipova <pavlina.antipova@gmail.com>
2021-11-08 20:05:31 +0200

    Исправляет скрипт

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,8 @@
 const validationInputRef = document.querySelector("#validation-input");
 
 const onValidationInput = (event) => {
-
     if (event.currentTarget.value.length === 6) {
-        return validationInputRef.classList.add("valid");
+        validationInputRef.classList.add("valid");
     } else {
         validationInputRef.classList.add("invalid");
     }
